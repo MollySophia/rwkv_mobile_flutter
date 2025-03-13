674363f1762687a5ef8407921f666ba7f37ba4af
WangCe <halowang1991@gmail.com>
2025-03-13 14:54:00 +0800

    _

--- example/lib/args.dart
+++ example/lib/args.dart
@@ -1,9 +1,7 @@
 abstract class Args {
   static const localeString = String.fromEnvironment("locale");
-  static const useFakeMessages = bool.fromEnvironment("use_fake_messages");
   static const enableChatDebugger = bool.fromEnvironment("enableChatDebugger");
   static const enableOthelloDebugger = bool.fromEnvironment("enable_othello_debugger");
-  static const hideDebugBanner = bool.fromEnvironment("hide_debug_banner", defaultValue: true);
   static const othelloTestCase = int.fromEnvironment("othello_test_case", defaultValue: -1);
-  static const demoType = String.fromEnvironment("demoType");
+  static const demoType = String.fromEnvironment("demoType", defaultValue: "chat");
 }
