b5dc55bc7b8272e71ebc89563326b54bdb00c158
Molly Sophia <mollysophia379@gmail.com>
2025-04-10 09:53:19 +0800

    update library
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- fetch_latest_libraries.sh
+++ fetch_latest_libraries.sh
@@ -3,13 +3,13 @@
 mkdir tmp && cd tmp
 wget https://github.com/MollySophia/rwkv-mobile/releases/download/latest/librwkv_mobile-dev-latest-ios-static.zip
 wget https://github.com/MollySophia/rwkv-mobile/releases/download/latest/librwkv_mobile-dev-latest-macos.zip
-wget https://github.com/MollySophia/rwkv-mobile/releases/download/latest/librwkv_mobile-dev-latest-aarch64-android-ndk-r25c.zip
+wget https://github.com/MollySophia/rwkv-mobile/releases/download/latest/librwkv_mobile-dev-latest-aarch64-android-ndk-r27c.zip
 wget https://github.com/MollySophia/rwkv-mobile/releases/download/latest/librwkv_mobile-dev-latest-linux-aarch64.zip
 wget https://github.com/MollySophia/rwkv-mobile/releases/download/latest/librwkv_mobile-dev-latest-linux-x86_64.zip
 
 mkdir ios && cd ios && unzip ../librwkv_mobile-dev-latest-ios-static.zip && cd ..
 mkdir macos && cd macos && unzip ../librwkv_mobile-dev-latest-macos.zip && cd ..
-mkdir android && cd android && unzip ../librwkv_mobile-dev-latest-aarch64-android-ndk-r25c.zip && cd ..
+mkdir android && cd android && unzip ../librwkv_mobile-dev-latest-aarch64-android-ndk-r27c.zip && cd ..
 mkdir linux && cd linux && unzip ../librwkv_mobile-dev-latest-linux-aarch64.zip && unzip ../librwkv_mobile-dev-latest-linux-x86_64.zip && cd ..
 cd ..
 
