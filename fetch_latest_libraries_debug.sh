4fb88a88e2d695169d9b747c5b8977728ffec3c7
Molly Sophia <mollysophia379@gmail.com>
2025-04-21 22:09:54 +0800

    fix __kmp_affinity_initialize crashing issue(hopefully)
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- fetch_latest_libraries_debug.sh
+++ fetch_latest_libraries_debug.sh
@@ -3,10 +3,10 @@
 mkdir tmp && cd tmp
 wget https://github.com/MollySophia/rwkv-mobile/releases/download/latest/librwkv_mobile-dev-latest-ios-static-debug.zip
 wget https://github.com/MollySophia/rwkv-mobile/releases/download/latest/librwkv_mobile-dev-latest-macos-debug.zip
-wget https://github.com/MollySophia/rwkv-mobile/releases/download/latest/librwkv_mobile-dev-latest-aarch64-android-ndk-r27c-debug.zip
+wget https://github.com/MollySophia/rwkv-mobile/releases/download/latest/librwkv_mobile-dev-latest-aarch64-android-ndk-r25c-debug.zip
 mkdir ios && cd ios && unzip ../librwkv_mobile-dev-latest-ios-static-debug.zip && cd ..
 mkdir macos && cd macos && unzip ../librwkv_mobile-dev-latest-macos-debug.zip && cd ..
-mkdir android && cd android && unzip ../librwkv_mobile-dev-latest-aarch64-android-ndk-r27c-debug.zip && cd ..
+mkdir android && cd android && unzip ../librwkv_mobile-dev-latest-aarch64-android-ndk-r25c-debug.zip && cd ..
 cd ..
 
 cp -r tmp/ios/* ios/
