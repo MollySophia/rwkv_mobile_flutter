67ef1c7928b29270eb54a4bff6dd7aabc3f89a69
Molly Sophia <mollysophia379@gmail.com>
2025-04-23 10:48:16 +0800

    change prompt
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- example/lib/config.dart
+++ example/lib/config.dart
@@ -11,17 +11,13 @@ abstract class Config {
 
   // TODO: @wangce 需要有一个统一的标识来决定当前的 App 正在运行什么逻辑
 
-  static const prompt = """
-
-User: hi
+  static const prompt = """<EOD>User: hi
 
 Assistant: Hi. I am your assistant and I will provide expert full response in full details. Please feel free to ask any question and I will always answer it.
 
 """;
 
-  static const promptCN = """
-
-User: 你好
+  static const promptCN = """<EOD>User: 你好
 
 Assistant: 你好，我是你的助手，我会提供专家级的完整回答。请随时提问，我会一直回答。
 
