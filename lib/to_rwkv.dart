d787d645d9d5b3cf44789d6876c7cb1a3dde36c4
WangCe <halowang1991@gmail.com>
2025-04-30 23:20:07 +0800

    Update to_rwkv.dart

--- lib/to_rwkv.dart
+++ lib/to_rwkv.dart
@@ -7,6 +7,8 @@ enum ToRWKV {
   getIsGenerating,
   getPrefillAndDecodeSpeed,
   getPrompt,
+
+  /// 获取responseBufferContentstop之后responseBufferContent还保留着，然后resume之后responseBufferContent会先短暂清空，然后变成stop前已经生成了的内容并接着生成
   getResponseBufferContent,
   getResponseBufferIds,
   getSamplerParams,
@@ -45,6 +47,8 @@ enum ToRWKV {
   setTokenBanned,
   setUserRole,
   setVisionPrompt,
+
+  /// stop之后responseBufferContent还保留着，然后resume之后responseBufferContent会先短暂清空，然后变成stop前已经生成了的内容并接着生成
   stop,
   ;
 }
