ca956dc60b4d2e41ed85687a0ce6c8865805a89e
Ce Wang <7344724+HaloWang@users.noreply.github.com>
2026-02-03 04:15:30 +0800

    refactor: update `forceLang` parameter to be non-nullable with default value
    - Changed `forceLang` in `ChatAsync` and `ChatBatchAsync` classes to be non-nullable.
    - Updated usage in `rwkv_mobile_flutter.dart` to remove null coalescing operator.
    - Added documentation for `forceLang` to clarify its purpose.

--- lib/to_rwkv.dart
+++ lib/to_rwkv.dart
@@ -295,7 +295,11 @@ class ChatAsync extends ToRWKV {
   final bool forceReasoning;
 
   final int? maxLength;
-  final int? forceLang;
+
+  /// 约束模型输出的第一个 token
+  ///
+  /// 0: 无限制, 1: 中文字符
+  final int forceLang;
 
   ChatAsync(
     this.messages, {
@@ -303,7 +307,7 @@ class ChatAsync extends ToRWKV {
     required this.forceReasoning,
     required this.modelID,
     this.maxLength,
-    this.forceLang,
+    this.forceLang = 0,
   });
 }
 
@@ -317,7 +321,11 @@ class ChatBatchAsync extends ToRWKV {
   final int batchSize;
 
   final int? maxLength;
-  final int? forceLang;
+
+  /// 约束模型输出的第一个 token
+  ///
+  /// 0: 无限制, 1: 中文字符
+  final int forceLang;
 
   ChatBatchAsync(
     this.messages, {
@@ -326,7 +334,7 @@ class ChatBatchAsync extends ToRWKV {
     required this.batchSize,
     required this.modelID,
     this.maxLength,
-    this.forceLang,
+    this.forceLang = 0,
   });
 }
 
